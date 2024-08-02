a518f0c6b5338638822dabf7446fb9d9604a3bc2
ori0nis <oihanegdcl@gmail.com>
2024-08-02 23:42:02 +0200

    started

---
+++ ejercicio4.js
@@ -0,0 +1,17 @@
+/* Crea una función llamada findArrayIndex que reciba como parametros un array de textos y un texto y devuelve la posición del array cuando el valor del array sea igual al valor del texto que enviaste como parámetro.
+
+Haz varios ejemplos y compruébalos. */
+
+function findArrayIndex(array, text) {
+    //Tu codigo
+}
+
+const mainCharacters = [
+  "Luke",
+  "Leia",
+  "Han Solo",
+  "Chewbacca",
+  "Rey",
+  "Anakin",
+  "Obi-Wan",
+];
